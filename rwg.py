92ab78936a433a13955a7638161f3281351562ec
Christopher Felli <christopher.felli@avanade.com>
2018-11-15 10:34:50 -0800

    Added passphrase

---
+++ rwg.py
@@ -0,0 +1,7 @@
+# rwg.py
+# Written by: ChrisTheCaribou
+# This program generates an n-length series of random words, which I use for
+# website logins. Also copies to clipboard!
+# Requires: pyperclip
+# > pip install pyperclip
+# > py -m pip install pyperclip
