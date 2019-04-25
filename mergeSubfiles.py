06fd2fcaaa5722f17320ed3befbebcff34760eee
Chris Felli <v-chfell@microsoft.com>
2019-04-25 16:22:45 -0700

    Formatted

--- mergeSubfiles.py
+++ mergeSubfiles.py
@@ -5,38 +5,45 @@
 # I)  [18, 5, 13]     13=11+2 is lowest of those costs
 # II) [18, 18]        18=5+13
 # III)[36]            36=18+18
-# IV) return 67       =13+18+36
+# IV) return 67       = 13+18+36
 
 total = 0
 final = 0
 iteration = 1
+
 def mergeSubfiles(arr):
     global total
     global iteration
     global final
 
-    # Edge case: length of one
+    # Edge case: Array length of one
     if len(arr)==1 & iteration==1:
         return 0
 
-    print("\nITERATION: ", iteration)
-    print("Inputed array is: ", arr)
+    print("Array is: ", arr)
+
+    # Define delta array, the derivative of arr
     delta = [0] * (len(arr)-1)
     for i in range(len(arr)-1):
         delta[i] = arr[i] + arr[i+1]
-    print("Delta array is: ", delta)
-    # find lowest index
+    #print("Delta array is: ", delta)
+    
+    # Find lowest index
     total += min(delta)
     minIndex = delta.index(min(delta))
-    print("Minimum position in array is: ", minIndex)
-    # At lowest index, 
+
+    # At lowest index, add next index's value and POP it
     arr[minIndex] = arr[minIndex]+arr[minIndex+1]
     arr.pop(minIndex+1)
+
     print("New array is: ", arr)
     print("Current total is: ", total)
     iteration += 1
+
+    # If array bigger than one, recurse
     if len(arr)!=1:
         mergeSubfiles(arr)
+    # Otherwise, reset variables and return final
     elif len(arr)==1:
         final = total
         total = 0
@@ -44,7 +51,7 @@ def mergeSubfiles(arr):
         print("FINAL TOTAL:", final)
     return final
 
-#mergeSubfiles([18, 5, 11, 2])
-#print(mergeSubfiles([18, 5, 11, 2]))
+# Testcases
+print(mergeSubfiles([18,5,11,2]))
 print(mergeSubfiles([18]))
-#print(mergeSubfiles([18]))
+print(mergeSubfiles([8,6,7,5,3,1,9]))
