7ec08a0c28737e3ed595eb0c59eb58a4befb5ebb
Chris Felli <cfelli317@gmail.com>
2019-05-05 13:11:37 -0700

    init framework

---
+++ LeetCode/LRUCache.py
@@ -0,0 +1,25 @@
+# CHRIS FELLI, 2019
+# Design an LRU Cache
+# It should support the following operations: get and put.
+# - get(key) - Get the value (will always be positive) of the key 
+#   if the key exists in the cache, otherwise return -1.
+# - put(key, value) - Set or insert the value if the key is not 
+#   already present. When the cache reached its capacity, it should
+#   invalidate the least recently used item before inserting a new item.
+
+class LRUCache:
+    
+    def __init__(self, capacity: int):
+        
+
+    def get(self, key: int) -> int:
+        
+
+    def put(self, key: int, value: int) -> None:
+        return None
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
