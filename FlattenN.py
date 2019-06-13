843fc8bd80a51410851de79b2178c563f53415c6
Chris Felli <v-chfell@microsoft.com>
2019-06-13 14:09:49 -0700

    flatten arrays

--- FlattenN.py
+++ FlattenN.py
@@ -2,16 +2,17 @@
 # Flatten a list of arrays of n depth
 from typing import Iterable            
 
-def flattenGenerator(items):
+def flattenGen(items):
     for x in items:
         if isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
-            for sub_x in flatten(x):
+            for sub_x in flattenGen(x):
                 yield sub_x
         else:
             yield x
 
 def flatten(items):
-    return list(flattenGenerator(items))
+    return list(flattenGen(items))
 
+# Driver
 print(flatten([[1, 2, 3], [4, [5, 6]], [7], [8, 9]]))
 print(flatten([[1, [2]], (3, 4, {5, 6}, 7), 8, "9"]))
