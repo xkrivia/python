ec4a2702defb0e96a9fd432576750558fd9f3eb3
Chris Felli <cfelli317@gmail.com>
2020-04-12 15:44:19 -0700

    added more romantoint test cases

--- src/leetcode/test_romantoint.py
+++ src/leetcode/test_romantoint.py
@@ -13,3 +13,53 @@ def romantoint(x):
 def test_i():
     """I is 1"""
     assert romantoint('I') == 1
+
+
+def test_ii():
+    """II is 2"""
+    assert romantoint('II') == 2
+
+
+def test_iii():
+    """III is 3"""
+    assert romantoint('III') == 3
+
+
+def test_iv():
+    """IV is 4"""
+    assert romantoint('IV') == 4
+
+
+def test_iiii():
+    """IIII is 4 for illiterate romans"""
+    assert romantoint('IIII') == 4
+
+
+def test_v():
+    """V is 5"""
+    assert romantoint('V') == 5
+
+
+def test_x():
+    """X is 10"""
+    assert romantoint('X') == 10
+
+
+def test_l():
+    """L is 50"""
+    assert romantoint('L') == 50
+
+
+def test_c():
+    """C is 100"""
+    assert romantoint('C') == 100
+
+
+def test_d():
+    """D is 500"""
+    assert romantoint('D') == 500
+
+
+def test_m():
+    """M is 1000"""
+    assert romantoint('M') == 1000
