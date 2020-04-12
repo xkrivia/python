046afb489d1ad6d7adf110fa5706411f3c307782
Chris Felli <cfelli317@gmail.com>
2020-04-12 16:34:53 -0700

    add twosum test cases

--- src/leetcode/test_twosum.py
+++ src/leetcode/test_twosum.py
@@ -1,28 +1,43 @@
-# CHRIS FELLI, 2019
-# Given an array of integers, return indices of the two numbers
-# such that they add up to a specific target.
-# nums = [2, 7, 11, 15], target = 9
-# Because nums[0] + nums[1] = 2 + 7 = 9,
-# return [0, 1].
-# You may assume that each input would have exactly one solution, 
-# and you may not use the same element twice.
 from typing import List
 
-class Solution:
-    def __init__(self, nums=[], target=0):
-        self.nums = nums
-        self.target = target
 
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        if len(nums) <= 1:
-            return False
-        buff_dict = {}
-        for i in range(len(nums)):
-            if nums[i] in buff_dict:
-                print(buff_dict)
-                return [buff_dict[nums[i]], i]
-            else:
-                buff_dict[target - nums[i]] = i
+def twosum(nums: List[int], target: int) -> List[int]:
+    if len(nums) <= 1:
+        return False
+    buff_dict = {}
+    for i in range(len(nums)):
+        if nums[i] in buff_dict:
+            print(buff_dict)
+            return [buff_dict[nums[i]], i]
+        else:
+            buff_dict[target - nums[i]] = i
 
-test = Solution()
-print(test.twoSum([1,2,3,4,5], 9))
+
+def test_1_2_3_4_5_target_9():
+    """[1, 2, 3, 4, 5] target 9 returns [4, 5] at indices [3, 4]"""
+    assert twosum([1, 2, 3, 4, 5], 9) == [3, 4]
+
+
+def test_1_target_9():
+    """[1] target 9 returns false"""
+    assert twosum([1], 9) is False
+
+
+def test_empty_target_9():
+    """[] target 9 returns false"""
+    assert twosum([], 9) is False
+
+
+def test_1_2_3_4_5_target_7():
+    """[1, 2, 3, 4, 5] target 7 returns [3, 4] at indices [2, 3]"""
+    assert twosum([1, 2, 3, 4, 5], 7) == [2, 3]
+
+
+def test_1_2_3_4_5_target_3():
+    """[1, 2, 3, 4, 5] target 3 returns [1, 2] at indices [0, 1]"""
+    assert twosum([1, 2, 3, 4, 5], 3) == [0, 1]
+
+
+def test_1_2_3_4_5_target_0():
+    """[1, 2, 3, 4, 5] target 0 returns false"""
+    assert twosum([1, 2, 3, 4, 5], 0) is None
