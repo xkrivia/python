dd01c7c3b3987efb461bd26bae8f2eb2e083e806
christhecaribou <dizzyquill@protonmail.com>
2020-04-18 17:24:22 -0700

    test_passphrase now copies code to the clipboard for you automatically

--- src/util/test_passphrase.py
+++ src/util/test_passphrase.py
@@ -16,6 +16,7 @@ def passphrase(length: int) -> str:
     alphabet = string.ascii_letters + string.digits + string.punctuation
     product = str(''.join(random.choice(alphabet) for i in range(length)))
     logging.info(" "+product)
+    pyperclip.copy(product)
     return(product)
 
 def test_passphrase_10_sets_of_10():
