a64687800c5279e6de7acca02b3fa0b27582aa67
christhecaribou <dizzyquill@protonmail.com>
2020-06-06 13:28:32 -0700

    changed unix_epoch to felli_time w/ combined ISO 8601 for casual readability

--- src/util/slogger.py
+++ src/util/slogger.py
@@ -1,18 +1,20 @@
 import logging
-import os
-import inspect
 import time
+import arrow
 
 
-def instantiate(file_name: str): 
-	"""
-	simple logging	
-	"""
-	file_dir, file_ext = "logs/", ".log"
-	full_log_file_path = file_dir + file_name + file_ext
-	
-	epoch_time = int(time.time())
-	logging.basicConfig(filename=full_log_file_path, level=logging.DEBUG)
-	logging.info("beginning slogger on " + str(epoch_time) + " ... ")	
-	
-	return None
+def instantiate(file_name: str) -> None:
+    """
+    simple logging
+    """
+    iso_8601 = arrow.now().format('YYYY-MM-DD')
+    unix_epoch = int(time.time())
+    felli_time = str(iso_8601)+"-"+str(unix_epoch)+"-"
+
+    file_dir, file_ext = "logs/", ".log"
+    full_log_file_path = file_dir+felli_time+file_name+file_ext
+
+    logging.basicConfig(filename=full_log_file_path, level=logging.DEBUG)
+    logging.info("beginning simple logger on " + felli_time + " ... ")
+
+    return None
