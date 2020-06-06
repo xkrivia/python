82438fe340dab7ca6c924f2372b17c8114ea564f
christhecaribou <dizzyquill@protonmail.com>
2020-06-06 13:32:17 -0700

    fixed slogger formatting

--- src/util/slogger.py
+++ src/util/slogger.py
@@ -9,12 +9,12 @@ def instantiate(file_name: str) -> None:
     """
     iso_8601 = arrow.now().format('YYYY-MM-DD')
     unix_epoch = int(time.time())
-    felli_time = str(iso_8601)+"-"+str(unix_epoch)+"-"
+    felli_time = str(iso_8601)+"-"+str(unix_epoch)
 
     file_dir, file_ext = "logs/", ".log"
-    full_log_file_path = file_dir+felli_time+file_name+file_ext
+    full_log_file_path = file_dir+felli_time+"-"+file_name+file_ext
 
     logging.basicConfig(filename=full_log_file_path, level=logging.DEBUG)
-    logging.info("beginning simple logger on " + felli_time + " ... ")
+    logging.info("beginning simple logger on "+felli_time+" ... ")
 
     return None
