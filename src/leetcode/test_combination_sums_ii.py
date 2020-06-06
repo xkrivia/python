80200ce2054372cfa8e639aec458eb4198988828
christhecaribou <dizzyquill@protonmail.com>
2020-06-06 16:19:01 -0700

    combination_sums_ii

---
+++ src/leetcode/test_combination_sums_ii.py
@@ -0,0 +1,50 @@
+import pytest
+
+
+class combination_sum_ii:
+    def __init__(self, candidates=[], target=0):
+        self.candidates = candidates
+        self.target = target
+
+    def combination_sum_ii(self, candidates, target):
+        candidates.sort()
+        result = []
+        self._combine_sum_ii(candidates, 0, [], result, target)
+        return result
+
+    def _combine_sum_ii(self, nums, start, path, result, target):
+        if not target:
+            result.append(path)
+            return
+
+        for i in range(start, len(nums)):
+            if i > start and nums[i] == nums[i - 1]:
+                continue
+            if nums[i] > target:
+                break
+            self._combine_sum_ii(nums, i + 1, path + [nums[i]],
+                                 result, target - nums[i])
+
+
+def test_combination_sum_ii_success():
+    test = combination_sum_ii()
+    assert(test.combination_sum_ii([10, 1, 2, 7, 6, 1, 5], 8))
+
+
+def test_combination_sum_ii_failures():
+    test = combination_sum_ii()
+    with pytest.raises(NameError):
+        test.combination_sum_ii(["text"], text)
+    with pytest.raises(TypeError):
+        test.combination_sum_ii(["2020 is wild"], 2)
+    with pytest.raises(TypeError):
+        test.combination_sum_ii(["2020 is wild"])
+    with pytest.raises(AttributeError):
+        test.combination_sum_ii(0, 1)
+
+def test_combination_sum_ii_edge_cases():
+    test = combination_sum_ii()
+    assert(test.combination_sum_ii([1], 1))
+    assert(test.combination_sum_ii([], 0))
+
+
