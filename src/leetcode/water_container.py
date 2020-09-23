49bd3abd53c9d58c50c9ded81b05489d186b5278
Zara <zfelli@protonmail.com>
2020-09-23 11:48:47 -0400

    finished water container

--- src/leetcode/water_container.py
+++ src/leetcode/water_container.py
@@ -1,3 +1,18 @@
 class Solution:
-    def maxArea(self, height: List[int]) -> int:
+    def maxArea(self, height):
         area = 0
+        i, j = 0, len(height) - 1
+        while i < j:
+            area_tracker = (j - i) * min(height[i], height[j])
+            if area_tracker >= area:
+                area = area_tracker
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+        return area
+
+
+def test_one():
+    assert Solution().maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]) == 49
+
